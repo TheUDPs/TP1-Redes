45bb336d1bb534ebbefca07abb676356dbacb7e9
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-04-12 16:43:48 -0300

    feat: add export method for topology

--- mininet/fragtopo.py
+++ mininet/fragtopo.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 from mininet.topo import Topo
+from mininet.net import Mininet
 
 
 class FragTopo(Topo):
@@ -22,6 +24,20 @@ class FragTopo(Topo):
         for i in range(client_number + 1):
             host_client_i = self.addHost(f"h{i + 1}")
             self.addLink(host_client_i, s3)
+
+    def export(self, filename="topology.dot"):
+        net = Mininet(topo=self)
+        with open(filename, "w") as f:
+            f.write("graph mininet {\n")
+            for host in net.hosts:
+                f.write(f"    {host.name} [shape=box];\n")
+            for switch in net.switches:
+                f.write(f"    {switch.name} [shape=circle];\n")
+            for link in net.links:
+                intf1, intf2 = link.intf1, link.intf2
+                node1, node2 = intf1.node, intf2.node
+                f.write(f"    {node1.name} -- {node2.name};\n")
+            f.write("}\n")
 
 
 topos = {"fragtopo": (lambda client_number=1: FragTopo(client_number))}
