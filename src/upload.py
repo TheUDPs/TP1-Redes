64f4b82b3d53b1358b953ed86c34f5506fd8623b
gabrieldiem <gabriel.diem5@gmail.com>
2025-04-13 16:24:54 -0300

    feat: added logger, and updated correct arguments to upload client

--- src/upload.py
+++ src/upload.py
@@ -1,92 +1,88 @@
 #!/usr/bin/env python3
+
 import argparse
-import sys
+from lib.constants import (
+    DEFAULT_PORT,
+    GO_BACK_N_PROTOCOL_TYPE,
+    STOP_AND_WAIT_PROTOCOL_TYPE,
+)
 
 
-def main():
+def parse_arguments():
     parser = argparse.ArgumentParser(
-        description="Script to upload files to a server with optional verbosity/quiet modes, "
-        "and a choice of protocol."
+        description="Client side application to upload files to the server side"
     )
 
-    parser.add_argument(
+    verbosity_group = parser.add_mutually_exclusive_group(required=False)
+
+    verbosity_group.add_argument(
         "-v",
+        "--verbose",
         action="store_true",
-        help="Enable verbose mode (print extra information during upload).",
+        help="increase output verbosiry",
     )
-    parser.add_argument(
+
+    verbosity_group.add_argument(
         "-q",
+        "--quiet",
         action="store_true",
-        help="Enable quiet mode (print as little output as possible).",
+        help="decrease output verbosiry",
     )
 
     parser.add_argument(
         "-H",
         "--host",
-        default="127.0.0.1",
+        required=True,
+        type=str,
         metavar="ADDR",
-        help="The server's IP address or hostname to which files are uploaded (default: 127.0.0.1).",
+        help="server IP address",
     )
+
     parser.add_argument(
         "-p",
         "--port",
+        required=False,
+        default=DEFAULT_PORT,
         type=int,
-        default=7001,
         metavar="PORT",
-        help="The port on the server to connect to (default: 7001).",
+        help="server port",
     )
 
     parser.add_argument(
         "-s",
-        "--filepath",
+        "--src",
         required=True,
+        type=str,
         metavar="FILEPATH",
-        help="The local path of the file to be uploaded.",
+        help="source file path",
     )
 
     parser.add_argument(
         "-n",
-        "--filename",
+        "--name",
         required=False,
+        type=str,
         metavar="FILENAME",
-        help="The name under which the file will be saved on the server.",
+        help="file name",
     )
 
     parser.add_argument(
-        "-t",
+        "-r",
         "--protocol",
-        choices=["stop-and-wait", "gbn"],
+        required=False,
+        choices=[STOP_AND_WAIT_PROTOCOL_TYPE, GO_BACK_N_PROTOCOL_TYPE],
+        default=GO_BACK_N_PROTOCOL_TYPE,
         metavar="PROTOCOL",
-        help="Protocol to use for file transfer. Allowed values: 'stop-and-wait' or 'gbn'.",
+        help="error recovery protocol",
     )
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    if args.v and args.q:
-        print(
-            "Error: You cannot use both -v (verbose) and -q (quiet) at the same time.",
-            file=sys.stderr,
-        )
-        sys.exit(1)
 
-    if args.v:
-        print("Arguments received:")
-        print(f"  Verbose Mode: {args.v}")
-        print(f"  Quiet Mode:   {args.q}")
-        print(f"  Host:         {args.host}")
-        print(f"  Port:         {args.port}")
-        print(f"  File Path:    {args.filepath}")
-        print(f"  File Name:    {args.filename}")
-        print(f"  Protocol:     {args.protocol}")
-
-    if not args.q:
-        print(
-            "File upload script is ready to upload the file with the specified arguments."
-        )
-        if args.filepath:
-            print(
-                f"Preparing to upload {args.filepath} to {args.host}:{args.port} as {args.filename} using {args.protocol}."
-            )
+def main():
+    args = parse_arguments()
+    print("Args received:")
+    print(args)
 
 
 if __name__ == "__main__":
