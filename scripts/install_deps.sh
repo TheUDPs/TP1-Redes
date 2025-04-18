e9df05dde960887e73d8f28b66486fb11992598a
gabrieldiem <gabriel.diem5@gmail.com>
2025-04-18 01:56:36 -0300

    feat: added script for installing deps to compile latex

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,3 +1,17 @@
+# A helper function to add color to a string message
+print_colored_message() {
+    shift
+    printf "\e[1;33m$@\e[0m\n"
+}
+
+print_colored_message "══ Installing mininet ══"
+
 sudo apt install mininet
+
+print_colored_message "══ Installing OpenVSwitch ══"
+
 sudo apt install openvswitch-testcontroller
+
+print_colored_message "══ Enabling OpenVSwitch ══"
+
 sudo systemctl enable --now openvswitch-switch
