5d5bd82c96aa1b2601c2037a75a17511cd793b88
gabrieldiem <gabriel.diem5@gmail.com>
2025-04-19 19:35:44 -0300

    feath & fix: topology with mut

--- mininet/linear_ends_topo.py
+++ mininet/linear_ends_topo.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python3
 from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.node import Node
 
 DEFAULT_PACKET_LOSS_PERCENTAGE = 0
 DEFAULT_CLIENT_NUMBER = 1
 
+DO_NOT_MODIFY_MTU = -1
+
 # because it applies twice the loss percentage (once per side)
 NORMALIZATION_FACTOR = 0.5
+
+
+class Router(Node):
+    def config(self, **params):
+        super(Router, self).config(**params)
+        self.cmd("sysctl -w net.ipv4.ip_forward=1")
+
+        self.cmd("ifconfig r1-eth0 10.0.0.254/24")
+        self.cmd("ifconfig r1-eth0 10.0.1.254/24")
+
+        # for i in range(0, client_number ):
+        #    self.cmd( f'ifconfig r1-eth0 10.0.{i}.254/24' )
+
+        # if mtu != DO_NOT_MODIFY_MTU:
+        self.cmd("ifconfig r1-eth0 mtu 600")
+
+    def terminate(self):
+        self.cmd("sysctl -w net.ipv4.ip_forward=0")
+        super(Router, self).terminate()
+
+
+class Host(Node):
+    def config(self, **params):
+        super(Host, self).config(**params)
+        self.cmd("sysctl -w net.ipv4.ip_no_pmtu_disc=1")
+        self.cmd("ip route flush cache")
+
+    def terminate(self):
+        super(Host, self).terminate()
 
 
 class LinearEndsTopo(Topo):
@@ -14,17 +46,20 @@ class LinearEndsTopo(Topo):
         self,
         client_number=DEFAULT_CLIENT_NUMBER,
         packet_loss_percentage=DEFAULT_PACKET_LOSS_PERCENTAGE,
+        mtu=DO_NOT_MODIFY_MTU,
     ):
         # add switches
         s1 = self.addSwitch("s1")
+        r1 = self.addHost("r1", cls=Router)
         s2 = self.addSwitch("s2")
-        s3 = self.addSwitch("s3")
 
-        h1_server = self.addHost("h1")
+        h1_server = self.addHost(
+            "h1", ip="10.0.0.1/24", defaultRoute="via 10.0.0.254", cls=Host
+        )
 
         # set links between switches
-        self.addLink(s1, s2)
-        self.addLink(s2, s3)
+        self.addLink(s1, r1)
+        self.addLink(r1, s2)
 
         normalized_loss = packet_loss_percentage * NORMALIZATION_FACTOR
 
@@ -34,8 +69,13 @@ class LinearEndsTopo(Topo):
         # set links for each client and the s3
         # 1 is added because the server is taken into account
         for i in range(1, client_number + 1):
-            host_client_i = self.addHost(f"h{i + 1}")
-            self.addLink(host_client_i, s3)
+            host_client_i = self.addHost(
+                f"h{i + 1}",
+                ip=f"10.0.{i}.1/24",
+                defaultRoute=f"via 10.0.{i}.254",
+                cls=Host,
+            )
+            self.addLink(host_client_i, s2)
 
     def export(self, filename="topology.dot"):
         net = Mininet(topo=self)
@@ -55,8 +95,9 @@ class LinearEndsTopo(Topo):
 topos = {
     "linends": (
         lambda client_number=DEFAULT_CLIENT_NUMBER,
-        packet_loss_percentage=DEFAULT_PACKET_LOSS_PERCENTAGE: LinearEndsTopo(
-            client_number, packet_loss_percentage
+        packet_loss_percentage=DEFAULT_PACKET_LOSS_PERCENTAGE,
+        mtu=DO_NOT_MODIFY_MTU: LinearEndsTopo(
+            client_number, packet_loss_percentage, mtu
         )
     )
 }
