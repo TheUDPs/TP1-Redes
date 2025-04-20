4902b2f8e0c7682a6b04661785ed2a379d32ba00
Jignacio14 <j14castro2001@gmail.com>
2025-04-19 22:05:48 -0300

    feat: Gracefull shutdown v.0

--- src/lib/server_lib/accepter.py
+++ src/lib/server_lib/accepter.py
@@ -1,16 +1,30 @@
+import threading
 from typing import *
 from threading import *
+import time 
 
 class Accepter:
     
-    def __init__(self):
+    def __init__(self, server_direction: tuple[str, int]):
         self.host_direction = ('127.0.0.0', 8080)
         self.is_alive: bool = True
+        self.thread_context: Thread = threading.Thread(target=self.run) 
+        
 
     def run(self):
         while self.is_alive:
-            self.accept()           
+            time.sleep(1)
+            new_client: int = self.accept()           
+        
 
     def accept(self):
-        print(f"Connection accepted from {self.host_direction}")
         return 0 
+
+    def kill(self): 
+        self.is_alive = False
+
+    def start(self):
+        self.thread_context.start()
+
+    def join(self):
+        self.thread_context.join()
--- src/lib/server_lib/server.py
+++ src/lib/server_lib/server.py
@@ -1,26 +1,32 @@
 from typing import *
+import threading
+from accepter import Accepter
 
 class Server:
 
-    def __init__(self) -> None:
-        self.acceptador = None
+    def __init__(self):
+        self.accepter: Accepter = Accepter()
         self.filesyste = None 
 
 
     def run(self):
         try:
-            # self.acceptador.run()
-            # self.filesystem.start()
-            while input() != 'q':
-                print("Server is running...")
-                pass
-            # self.acceptador.finalize()
-            # self.filesyste.finalize()
+            self.accepter.start()
+            print("Estoy aca esperando")
+
+            while True: 
+                user_input = input()
+                print("Proceso input")
+                if user_input.strip().lower() == "q":
+                    print("Hago el break")
+                    break
+                print("No hice nada")
+            self.accepter.kill()
+            self.accepter.join()
             return 0
         except Exception as e:
             print(e)
             return -1 
-
 
 
 if __name__ == "__main__":
