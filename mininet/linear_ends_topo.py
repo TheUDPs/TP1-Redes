03723bdc20d45581b9c1d77e507c7dd56e55fd6e
gabrieldiem <gabriel.diem5@gmail.com>
2025-04-20 16:07:17 -0300

    intermediate results with topo

--- mininet/linear_ends_topo.py
+++ mininet/linear_ends_topo.py
@@ -6,6 +6,7 @@ from mininet.topo import Topo
 DEFAULT_PACKET_LOSS_PERCENTAGE = 0
 DEFAULT_CLIENT_NUMBER = 1
 DO_NOT_MODIFY_MTU = -1
+ETH_NUMBER_NOT_SET = -1
 
 # because it applies twice the loss percentage (once per side)
 NORMALIZATION_FACTOR = 0.5
@@ -17,31 +18,45 @@ class Router(Node):
 
         # to let the node act as router
         self.cmd("sysctl -w net.ipv4.ip_forward=1")
+        self.cmd("sysctl -w net.ipv4.ip_no_pmtu_disc=1")
 
-        # configure eth0 interface to handle server subnet (10.0.0.0/24)
-        # will use 10.0.0.254 as its IP on this subnet
-        self.cmd(f"ifconfig {self.name}-eth0 10.0.0.254/24")
+        # configure eth0 interface for server subnet
+        self.cmd(f"ip addr add 10.0.0.254/24 dev {self.name}-eth0")
+        self.cmd(f"ip link set {self.name}-eth0 up")
 
-        # configure eth1 interface to handle clients' subnet (10.0.1.0/24)
-        # will use 10.0.1.254 as its IP on this subnet
-        self.cmd(f"ifconfig {self.name}-eth1 10.0.1.254/24")
+        # configure eth1 interface for clients' subnet
+        self.cmd(f"ip addr add 10.0.1.254/24 dev {self.name}-eth1")
+        self.cmd(f"ip link set {self.name}-eth1 up")
 
-        # add explicit routes for the server subnet via eth0
-        # so packets destined for 10.0.0.0/24 go through eth0
+        # add routes
         self.cmd(f"ip route add 10.0.0.0/24 dev {self.name}-eth0")
-
-        # add explicit routes for the clients' subnet via eth1
-        # so packets destined for 10.0.1.0/24 go through eth1
         self.cmd(f"ip route add 10.0.1.0/24 dev {self.name}-eth1")
 
         # if MTU is set to change apply the change only to eth0 side
         mtu = params.get("mtu", DO_NOT_MODIFY_MTU)
-        if mtu != DO_NOT_MODIFY_MTU and mtu > 0:
-            # self.cmd(f"ifconfig {self.name}-eth0 mtu {mtu}")
-            self.cmd(f"ip link set dev {self.name}-eth0 mtu {mtu}")
+        eth_number = params.get("eth_number", ETH_NUMBER_NOT_SET)
+        if mtu != DO_NOT_MODIFY_MTU and mtu > 0 and eth_number != ETH_NUMBER_NOT_SET:
+            print(f"[MTU: {mtu}]")
+            self.cmd(f"ip link set dev {self.name}-eth{eth_number} mtu {mtu}")
+
+        # allow all ICMP messages
+        self.cmd("iptables -A INPUT -p icmp -j ACCEPT")
+        self.cmd("iptables -A OUTPUT -p icmp -j ACCEPT")
+        self.cmd("iptables -A FORWARD -p icmp -j ACCEPT")
+        self.cmd(
+            "iptables -I OUTPUT -p icmp --icmp-type fragmentation-needed -j ACCEPT"
+        )
 
     def terminate(self):
         self.cmd("sysctl -w net.ipv4.ip_forward=0")
+
+        # bring interfaces down and remove IPs
+        self.cmd(f"ip addr flush dev {self.name}-eth0")
+        self.cmd(f"ip link set {self.name}-eth0 down")
+
+        self.cmd(f"ip addr flush dev {self.name}-eth1")
+        self.cmd(f"ip link set {self.name}-eth1 down")
+
         super(Router, self).terminate()
 
 
@@ -66,12 +81,19 @@ class LinearEndsTopo(Topo):
     ):
         # add link-layer switches & router
         s1 = self.addSwitch("s1")
-        r1 = self.addNode("r1", cls=Router, client_number=client_number, mtu=mtu)
-        s2 = self.addSwitch("s2")
+        s2 = self.addNode(
+            "s2",
+            ip="10.0.0.254/24",
+            cls=Router,
+            client_number=client_number,
+            mtu=mtu,
+            eth_number=0,
+        )
+        s3 = self.addSwitch("s3")
 
         # set links between switches & router
-        self.addLink(s1, r1)
-        self.addLink(r1, s2)
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
 
         # availbale at IP 10.0.0.1/24
         # default route via 10.0.0.254 (router's eth0 interface)
@@ -94,7 +116,7 @@ class LinearEndsTopo(Topo):
                 defaultRoute="via 10.0.1.254",  # router's eth1 interface
                 cls=Host,
             )
-            self.addLink(host_client_i, s2)
+            self.addLink(host_client_i, s3)
 
     def export(self, filename="topology.dot"):
         net = Mininet(topo=self)
