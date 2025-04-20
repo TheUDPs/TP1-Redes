2742525ad2cc10a258e9bf5c66b2e69ffd08d38f
Jignacio14 <j14castro2001@gmail.com>
2025-04-19 22:10:39 -0300

    fix: imports + types

--- src/lib/server_lib/accepter.py
+++ src/lib/server_lib/accepter.py
@@ -1,26 +1,21 @@
-import threading
-from typing import *
-from threading import *
-import time 
+from threading import Thread
+import time
+
 
 class Accepter:
-    
     def __init__(self, server_direction: tuple[str, int]):
-        self.host_direction = ('127.0.0.0', 8080)
+        self.host_direction: tuple[str, int] = server_direction
         self.is_alive: bool = True
-        self.thread_context: Thread = threading.Thread(target=self.run) 
-        
+        self.thread_context: Thread = Thread(target=self.run)
 
     def run(self):
         while self.is_alive:
             time.sleep(1)
-            new_client: int = self.accept()           
-        
 
     def accept(self):
-        return 0 
+        return 0
 
-    def kill(self): 
+    def kill(self):
         self.is_alive = False
 
     def start(self):
--- src/lib/server_lib/server.py
+++ src/lib/server_lib/server.py
@@ -1,36 +1,29 @@
-from typing import *
-import threading
 from accepter import Accepter
 
+CLOSING_KEY = "q"
+
+
 class Server:
-
     def __init__(self):
-        self.accepter: Accepter = Accepter()
-        self.filesyste = None 
-
+        self.accepter: Accepter = Accepter(("127.0.0", 8080))
+        self.filesyste = None
 
     def run(self):
         try:
             self.accepter.start()
-            print("Estoy aca esperando")
-
-            while True: 
+            while True:
                 user_input = input()
-                print("Proceso input")
-                if user_input.strip().lower() == "q":
-                    print("Hago el break")
+                if user_input.strip().lower() == CLOSING_KEY:
                     break
-                print("No hice nada")
             self.accepter.kill()
             self.accepter.join()
             return 0
         except Exception as e:
             print(e)
-            return -1 
+            return -1
 
 
 if __name__ == "__main__":
     server: Server = Server()
     server.run()
     print("Server finished.")
-    
