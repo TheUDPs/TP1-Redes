d6baf360933b95911c859c642c8941b325269497
gabrieldiem <gabriel.diem5@gmail.com>
2025-04-23 23:47:52 -0300

    fix: moved upload client arg parsing to its own class

--- src/upload.py
+++ src/upload.py
@@ -1,89 +1,15 @@
 #!/usr/bin/env python3
 
-import argparse
-from lib.constants import (
-    DEFAULT_PORT,
-    GO_BACK_N_PROTOCOL_TYPE,
-    STOP_AND_WAIT_PROTOCOL_TYPE,
-)
+from lib.client.parser_upload import ClientUploadArgParser
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description="Client side application to upload files to the server side"
-    )
+def upload():
+    arg_parser = ClientUploadArgParser()
+    _args = arg_parser.parse()  # noqa: F841
 
-    verbosity_group = parser.add_mutually_exclusive_group(required=False)
-
-    verbosity_group.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        help="increase output verbosity",
-    )
-
-    verbosity_group.add_argument(
-        "-q",
-        "--quiet",
-        action="store_true",
-        help="decrease output verbosity",
-    )
-
-    parser.add_argument(
-        "-H",
-        "--host",
-        required=True,
-        type=str,
-        metavar="ADDR",
-        help="server IP address",
-    )
-
-    parser.add_argument(
-        "-p",
-        "--port",
-        required=False,
-        default=DEFAULT_PORT,
-        type=int,
-        metavar="PORT",
-        help="server port",
-    )
-
-    parser.add_argument(
-        "-s",
-        "--src",
-        required=True,
-        type=str,
-        metavar="FILEPATH",
-        help="source file path",
-    )
-
-    parser.add_argument(
-        "-n",
-        "--name",
-        required=False,
-        type=str,
-        metavar="FILENAME",
-        help="file name on the server",
-    )
-
-    parser.add_argument(
-        "-r",
-        "--protocol",
-        required=False,
-        choices=[STOP_AND_WAIT_PROTOCOL_TYPE, GO_BACK_N_PROTOCOL_TYPE],
-        default=GO_BACK_N_PROTOCOL_TYPE,
-        metavar="PROTOCOL",
-        help="error recovery protocol",
-    )
-
-    return parser.parse_args()
-
-
-def main():
-    args = parse_arguments()
-    print("Args received:")
-    print(args)
+    print("Client started for upload")
+    print("Shutdown")
 
 
 if __name__ == "__main__":
-    main()
+    upload()
