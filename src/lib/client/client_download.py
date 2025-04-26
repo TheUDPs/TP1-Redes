cf731927bb8a3559b9ec56105d0d6faf26255f10
Jignacio14 <j14castro2001@gmail.com>
2025-04-26 15:48:42 -0300

    feat: file download skeleton

--- src/lib/client/client_download.py
+++ src/lib/client/client_download.py
@@ -1,5 +1,6 @@
 from lib.client.abstract_client import Client
 from lib.common.logger import Logger
+from src.lib.common.constants import DOWNLOAD_OPERATION
 
 
 class DownloadClient(Client):
@@ -13,7 +14,40 @@ class DownloadClient(Client):
 
     def perform_download(self) -> None:
         try:
-            pass
+            self.operation_intention()
+            self.receive_file_info()
+            self.receive_file()
         except Exception as e:
             err = e.message if e.message else e
             self.logger.error(f"Error message: {err}")
+
+    def receive_file_info(self) -> None:
+        try:
+            self.logger.debug("Receiving file info")
+            self.send_file_name()
+
+        except Exception:
+            pass
+
+    def send_file_name(self):
+        pass
+
+    def receive_file(self) -> None:
+        self.logger.debug("Receiving file info")
+        try:
+            pass
+        except Exception:
+            pass
+
+    def send_operation_intention(self) -> None:
+        self.logger.debug("[CONN] Sending operation intention")
+        try:
+            self.protocol.send_operation_intention(
+                self.sequence_number, DOWNLOAD_OPERATION
+            )
+            self.protocol.wait_for_operation_confirmation(self.sequence_number)
+            self.logger.debug("Operation accepted")
+        except Exception:
+            pass
+        except Exception:
+            pass
