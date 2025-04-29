a3dd8bdba7cd242d9396b52c97b04cf37da20de8
Jignacio14 <j14castro2001@gmail.com>
2025-04-28 22:24:20 -0300

    feat: add skeleton for new upload method

--- src/lib/client/client_upload_gbn.py
+++ src/lib/client/client_upload_gbn.py
@@ -3,7 +3,12 @@ from os import getcwd, path
 from lib.client.abstract_client import Client
 from lib.client.exceptions.file_already_exists import FileAlreadyExists
 from lib.client.exceptions.file_too_big import FileTooBig
-from lib.common.constants import ERROR_EXIT_CODE, UPLOAD_OPERATION
+from lib.common.constants import (
+    ERROR_EXIT_CODE,
+    FILE_CHUNK_SIZE,
+    UPLOAD_OPERATION,
+    WINDOWS_SIZE,
+)
 from lib.common.exceptions.connection_lost import ConnectionLost
 from lib.common.exceptions.invalid_filename import InvalidFilename
 from lib.common.exceptions.message_not_ack import MessageIsNotAck
@@ -19,7 +24,7 @@ class UploadClient(Client):
     ):
         self.src_filepath: str = src
         self.filename_in_server: str = name
-
+        self.windows_size: int = WINDOWS_SIZE
         try:
             self.file_handler: FileHandler = FileHandler(getcwd(), logger)
             self.file = self.file_handler.open_file_read_mode(
@@ -93,6 +98,34 @@ class UploadClient(Client):
             raise FileTooBig()
 
     def send_file(self) -> None:
+        total_chunks: int = self.file_handler.get_number_of_chunks(
+            self.filesize, FILE_CHUNK_SIZE
+        )
+
+        # To do: es la mejor forma ?? Leo todos los chuncks no tener que releer el archivo en caso de retransmision
+        chunk_list: list[bytes] = []
+
+        for i in range(total_chunks):
+            chunk = self.file_handler.read(self.file, FILE_CHUNK_SIZE)
+            chunk_list.append(chunk)
+
+        self.logger.force_info("File transfer complete")
+        self.file_handler.close(self.file)
+
+    def transmit_chunk_phase():
+        # si puedo paso el chuck
+        # paso la cantidad que tenga la posibilidad de compartir windows size - on fly
+        # si no puedo continuo
+        pass
+
+    def await_ack_phase():
+        # espero el ack
+        # si no llegaa
+        # hago time out y reenvio todo desde la base de la windows
+        # si llega el ack
+        # si es nuevo ack registro y continuo
+        # si no es nuevo ack (es ack repetido) amplio la cuenta hasta llegar a 4. Si llega a 4 retransmitir toda la ventada
+
         pass
 
     def closing_handshake(self) -> None:
