688ee066762cc9b394cf522f10339958601a7d34
Jignacio14 <j14castro2001@gmail.com>
2025-05-04 16:03:08 -0300

    feat: 3 downloads + 1 upload

--- tests/test_multiple_clients.py
+++ tests/test_multiple_clients.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from time import sleep
 
@@ -10,6 +11,7 @@ from mininet_topo.linear_ends_topo import LinearEndsTopo
 from tests.common import (
     TESTS_DIR,
     check_results,
+    compute_sha256,
     emergency_directory_teardown,
     setup_directories,
     get_random_port,
@@ -95,30 +97,52 @@ def test_01_server_can_handle_correctly_3_downloads_and_3_uploads_simultaneously
 
     # Downloaders
     h5 = mininet_net_setup.get("h5")
-    # h6 = mininet_net_setup.get("h6")
-    # h7 = mininet_net_setup.get("h7")
+    h6 = mininet_net_setup.get("h6")
+    h7 = mininet_net_setup.get("h7")
 
     _p_loss = P_LOSS.value
 
-    tmp_dirpath, timestamp = setup_directories(TESTS_DIR)
-    filepath_for_upload = f"{tmp_dirpath}/test_file_upload1.txt"
-    filepath_for_download = f"{tmp_dirpath}/server/test_file_download.txt"
-    generate_random_text_file(filepath_for_upload, 1)
-    generate_random_text_file(filepath_for_download, 1)
+    tmp_dirpath1, timestamp = setup_directories(TESTS_DIR)
+    tmp_dirpath2, timestamp = setup_directories(TESTS_DIR)
+    tmp_dirpath3, timestamp = setup_directories(TESTS_DIR)
+    filepath_for_upload = f"{tmp_dirpath1}/test_file_upload1.txt"
+    filepath_for_download = f"{tmp_dirpath1}/server/test_file_download.txt"
+    generate_random_text_file(filepath_for_upload, 5)
+    generate_random_text_file(filepath_for_download, 5)
 
     port = get_random_port()
 
     print(f"Starting server on {h1.name}...")
-    server_pid, server_log = start_server(h1, tmp_dirpath, port, PROTOCOL.value)
+    server_pid, server_log = start_server(h1, tmp_dirpath1, port, PROTOCOL.value)
 
     sleep(1)  # wait for server start
 
     dw_cltn_1_pid, dw_cltn_1_log = start_download_client(
-        h5, tmp_dirpath, port, PROTOCOL.value, file_to_download="test_file_download.txt"
+        h5,
+        tmp_dirpath1,
+        port,
+        PROTOCOL.value,
+        file_to_download="test_file_download.txt",
+    )
+
+    dw_cltn_2_pid, dw_cltn_2_log = start_download_client(
+        h6,
+        tmp_dirpath2,
+        port,
+        PROTOCOL.value,
+        file_to_download="test_file_download.txt",
+    )
+
+    dw_cltn_3_pid, dw_cltn_3_log = start_download_client(
+        h7,
+        tmp_dirpath3,
+        port,
+        PROTOCOL.value,
+        file_to_download="test_file_download.txt",
     )
 
     up_cltn_1_pid, up_cltn_1_log = start_upload_client(
-        h5, tmp_dirpath, port, PROTOCOL.value, file_to_upload=filepath_for_upload
+        h5, tmp_dirpath1, port, PROTOCOL.value, file_to_upload=filepath_for_upload
     )
 
     # pids_and_logs_uploaders = start_all_upload_clients(
@@ -143,8 +167,8 @@ def test_01_server_can_handle_correctly_3_downloads_and_3_uploads_simultaneously
     ]
     were_client_downloaders_successful = [
         MutableVariable(False),
-        # MutableVariable(False),
-        # MutableVariable(False),
+        MutableVariable(False),
+        MutableVariable(False),
     ]
     was_server_successful = MutableVariable(False)
 
@@ -176,6 +200,26 @@ def test_01_server_can_handle_correctly_3_downloads_and_3_uploads_simultaneously
         p_loss=_p_loss,
     )
 
+    check_results(
+        was_client_successful=were_client_downloaders_successful[1],
+        was_server_successful=was_server_successful,
+        client_log=dw_cltn_2_log,
+        server_log=server_log,
+        server_message_expected=_server_message_expected,
+        client_message_expected=_client_downloader_message_expected,
+        p_loss=_p_loss,
+    )
+
+    check_results(
+        was_client_successful=were_client_downloaders_successful[2],
+        was_server_successful=was_server_successful,
+        client_log=dw_cltn_2_log,
+        server_log=server_log,
+        server_message_expected=_server_message_expected,
+        client_message_expected=_client_downloader_message_expected,
+        p_loss=_p_loss,
+    )
+
     print("Cleaning up processes...")
     kill_process(h1, server_pid)
 
@@ -184,6 +228,8 @@ def test_01_server_can_handle_correctly_3_downloads_and_3_uploads_simultaneously
     # kill_process(h4, pids_and_logs_uploaders["h4"]["pid"])
 
     kill_process(h5, dw_cltn_1_pid)
+    kill_process(h6, dw_cltn_2_pid)
+    kill_process(h7, dw_cltn_3_pid)
     # kill_process(h6, pids_and_logs_downloaders["h6"]["pid"])
     # kill_process(h7, pids_and_logs_downloaders["h7"]["pid"])
 
@@ -195,17 +241,49 @@ def test_01_server_can_handle_correctly_3_downloads_and_3_uploads_simultaneously
     # print_outputs(server_log, pids_and_logs_downloaders["h6"]["log"])
     # print_outputs(server_log, pids_and_logs_downloaders["h7"]["log"])
 
-    teardown_directories(tmp_dirpath)
+    assert os.path.exists(
+        f"{tmp_dirpath1}/client/test_file_download.txt"
+    ) and os.path.exists(filepath_for_download)
 
-    # assert was_server_successful.value
+    assert os.path.exists(filepath_for_upload) and os.path.exists(
+        f"{tmp_dirpath1}/server/test_file_upload1.txt"
+    )
+
+    hash_client_1 = compute_sha256(f"{tmp_dirpath1}/client/test_file_download.txt")
+    hash_client_1_up = compute_sha256(f"{tmp_dirpath1}/test_file_upload1.txt")
+    hash_client_2 = compute_sha256(f"{tmp_dirpath2}/client/test_file_download.txt")
+    hash_client_3 = compute_sha256(f"{tmp_dirpath3}/client/test_file_download.txt")
+    hash_server = compute_sha256(filepath_for_download)
+    hash_server_up = compute_sha256(f"{tmp_dirpath1}/server/test_file_upload1.txt")
+
+    assert hash_client_1 == hash_server, (
+        "SHA256 mismatch: downloaded file is not identical to the original"
+    )
+    assert hash_client_2 == hash_server, (
+        "SHA256 mismatch: downloaded file is not identical to the original"
+    )
+
+    assert hash_client_3 == hash_server, (
+        "SHA256 mismatch: downloaded file is not identical to the original"
+    )
+
+    assert hash_client_1_up == hash_server_up, (
+        "SHA256 mismatch: uploaded file is not identical to the original"
+    )
+
+    teardown_directories(tmp_dirpath1)
+    teardown_directories(tmp_dirpath2)
+    teardown_directories(tmp_dirpath3)
+
+    assert was_server_successful.value
 
     assert were_client_uploaders_successful[0].value
     # assert were_client_uploaders_successful[1].value
     # assert were_client_uploaders_successful[2].value
 
     assert were_client_downloaders_successful[0].value
-    # assert were_client_downloaders_successful[1].value
-    # assert were_client_downloaders_successful[2].value
+    assert were_client_downloaders_successful[1].value
+    assert were_client_downloaders_successful[2].value
 
     # if os.path.exists(client_log) and os.path.exists(server_log):
     #     hash_client = compute_sha256(client_log)
