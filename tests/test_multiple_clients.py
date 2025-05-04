fc52ec4241faf3984412de63db28e7898dc75a5a
Jignacio14 <j14castro2001@gmail.com>
2025-05-04 14:35:07 -0300

    feat: Intento agregar pruebas para varios usuarios

--- tests/test_multiple_clients.py
+++ tests/test_multiple_clients.py
@@ -9,13 +9,13 @@ from src.lib.common.mutable_variable import MutableVariable
 from mininet_topo.linear_ends_topo import LinearEndsTopo
 from tests.common import (
     TESTS_DIR,
+    check_results,
     emergency_directory_teardown,
     setup_directories,
     get_random_port,
     start_server,
     start_download_client,
     kill_process,
-    print_outputs,
     teardown_directories,
     start_upload_client,
     generate_random_text_file,
@@ -26,7 +26,7 @@ PROTOCOL = MutableVariable("saw")
 
 
 @pytest.fixture(
-    scope="module", params=["saw;0", "saw;10", "saw;40"]
+    scope="module", params=["saw;0", "saw;10", "saw;40", "gbn;0", "gbn;10", "gbn;40"]
 )  # Pending to add: "gbn;0", "gbn;10", "gbn;40"
 def mininet_net_setup(request):
     protocol = request.param.split(";")[0]
@@ -36,7 +36,7 @@ def mininet_net_setup(request):
     PROTOCOL.value = protocol
 
     topo = LinearEndsTopo(
-        client_number=6, packet_loss_percentage=packet_loss_percentage
+        client_number=7, packet_loss_percentage=packet_loss_percentage
     )
     net = Mininet(topo=topo, link=TCLink)
     print(
@@ -82,29 +82,29 @@ def start_all_download_clients(
     return pids_and_logs
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_01_server_can_handle_correctly_3_downloads_and_3_uploads_simultaneously(
     mininet_net_setup,
 ):
     h1 = mininet_net_setup.get("h1")
 
     # Uploaders
-    h2 = mininet_net_setup.get("h2")
-    h3 = mininet_net_setup.get("h3")
-    h4 = mininet_net_setup.get("h4")
+    # h2 = mininet_net_setup.get("h2")
+    # h3 = mininet_net_setup.get("h3")
+    # h4 = mininet_net_setup.get("h4")
 
     # Downloaders
     h5 = mininet_net_setup.get("h5")
-    h6 = mininet_net_setup.get("h6")
-    h7 = mininet_net_setup.get("h7")
+    # h6 = mininet_net_setup.get("h6")
+    # h7 = mininet_net_setup.get("h7")
 
     _p_loss = P_LOSS.value
 
     tmp_dirpath, timestamp = setup_directories(TESTS_DIR)
-    filepath_for_upload = f"{tmp_dirpath}/test_file_upload.txt"
+    filepath_for_upload = f"{tmp_dirpath}/test_file_upload1.txt"
     filepath_for_download = f"{tmp_dirpath}/server/test_file_download.txt"
-    generate_random_text_file(filepath_for_upload)
-    generate_random_text_file(filepath_for_download)
+    generate_random_text_file(filepath_for_upload, 1)
+    generate_random_text_file(filepath_for_download, 1)
 
     port = get_random_port()
 
@@ -113,30 +113,34 @@ def test_01_server_can_handle_correctly_3_downloads_and_3_uploads_simultaneously
 
     sleep(1)  # wait for server start
 
-    pids_and_logs_uploaders = start_all_upload_clients(
-        [h2, h3, h4],
-        tmp_dirpath,
-        port,
-        PROTOCOL.value,
-        file_to_upload=filepath_for_upload,
-    )
-    pids_and_logs_downloaders = start_all_download_clients(
-        [h5, h6, h7],
-        tmp_dirpath,
-        port,
-        PROTOCOL.value,
-        file_to_download=filepath_for_download,
+    dw_cltn_1_pid, dw_cltn_1_log = start_download_client(
+        h5, tmp_dirpath, port, PROTOCOL.value, file_to_download="test_file_download.txt"
     )
 
-    were_client_uploaders_successful = [
-        MutableVariable(False),
-        MutableVariable(False),
-        MutableVariable(False),
-    ]
+    # pids_and_logs_uploaders = start_all_upload_clients(
+    #     [h2],
+    #     tmp_dirpath,
+    #     port,
+    #     PROTOCOL.value,
+    #     file_to_upload=filepath_for_upload,
+    # )
+    # pids_and_logs_downloaders = start_all_download_clients(
+    #     [h5],
+    #     tmp_dirpath,
+    #     port,
+    #     PROTOCOL.value,
+    #     file_to_download=filepath_for_download,
+    # )
+
+    # # were_client_uploaders_successful = [
+    # #     MutableVariable(False),
+    #     # MutableVariable(False),
+    #     # MutableVariable(False),
+    # ]
     were_client_downloaders_successful = [
         MutableVariable(False),
-        MutableVariable(False),
-        MutableVariable(False),
+        # MutableVariable(False),
+        # MutableVariable(False),
     ]
     was_server_successful = MutableVariable(False)
 
@@ -148,54 +152,64 @@ def test_01_server_can_handle_correctly_3_downloads_and_3_uploads_simultaneously
         "Download completed to client",
     ]
 
-    # check_results_multiple_clients(
-    #     was_client_successful=was_client_successful,
+    # check_results(
+    #     was_client_successful=were_client_uploaders_successful[0],
     #     was_server_successful=was_server_successful,
-    #     client_log=client_log,
+    #     client_log=["h2"]["log"],
     #     server_log=server_log,
-    #     server_message_expected=server_message_expected,
-    #     client_message_expected=client_message_expected,
-    #     p_loss=p_loss,
+    #     server_message_expected=_server_message_expected,
+    #     client_message_expected=_client_uploader_message_expected,
+    #     p_loss=_p_loss,
     # )
+
+    check_results(
+        was_client_successful=were_client_downloaders_successful[0],
+        was_server_successful=was_server_successful,
+        client_log=dw_cltn_1_log,
+        server_log=server_log,
+        server_message_expected=_server_message_expected,
+        client_message_expected=_client_downloader_message_expected,
+        p_loss=_p_loss,
+    )
 
     print("Cleaning up processes...")
     kill_process(h1, server_pid)
 
-    kill_process(h2, pids_and_logs_uploaders["h2"]["pid"])
-    kill_process(h3, pids_and_logs_uploaders["h3"]["pid"])
-    kill_process(h4, pids_and_logs_uploaders["h4"]["pid"])
+    # kill_process(h2, pids_and_logs_uploaders["h2"]["pid"])
+    # kill_process(h3, pids_and_logs_uploaders["h3"]["pid"])
+    # kill_process(h4, pids_and_logs_uploaders["h4"]["pid"])
 
-    kill_process(h5, pids_and_logs_downloaders["h5"]["pid"])
-    kill_process(h6, pids_and_logs_downloaders["h6"]["pid"])
-    kill_process(h7, pids_and_logs_downloaders["h7"]["pid"])
+    kill_process(h5, dw_cltn_1_pid)
+    # kill_process(h6, pids_and_logs_downloaders["h6"]["pid"])
+    # kill_process(h7, pids_and_logs_downloaders["h7"]["pid"])
 
-    print_outputs(server_log, pids_and_logs_uploaders["h2"]["log"])
-    print_outputs(server_log, pids_and_logs_uploaders["h3"]["log"])
-    print_outputs(server_log, pids_and_logs_uploaders["h4"]["log"])
+    # print_outputs(server_log, pids_and_logs_uploaders["h2"]["log"])
+    # print_outputs(server_log, pids_and_logs_uploaders["h3"]["log"])
+    # print_outputs(server_log, pids_and_logs_uploaders["h4"]["log"])
 
-    print_outputs(server_log, pids_and_logs_downloaders["h5"]["log"])
-    print_outputs(server_log, pids_and_logs_downloaders["h6"]["log"])
-    print_outputs(server_log, pids_and_logs_downloaders["h7"]["log"])
+    # print_outputs(server_log, pids_and_logs_downloaders["h5"]["log"])
+    # print_outputs(server_log, pids_and_logs_downloaders["h6"]["log"])
+    # print_outputs(server_log, pids_and_logs_downloaders["h7"]["log"])
 
     teardown_directories(tmp_dirpath)
 
-    assert was_server_successful.value
+    # assert was_server_successful.value
 
-    assert were_client_uploaders_successful[0].value
-    assert were_client_uploaders_successful[1].value
-    assert were_client_uploaders_successful[2].value
+    # assert were_client_uploaders_successful[0].value
+    # assert were_client_uploaders_successful[1].value
+    # assert were_client_uploaders_successful[2].value
 
     assert were_client_downloaders_successful[0].value
-    assert were_client_downloaders_successful[1].value
-    assert were_client_downloaders_successful[2].value
+    # assert were_client_downloaders_successful[1].value
+    # assert were_client_downloaders_successful[2].value
 
     # if os.path.exists(client_log) and os.path.exists(server_log):
     #     hash_client = compute_sha256(client_log)
     #     hash_server = compute_sha256(server_log)
-    #
+
     #     print(f"Client file hash: {hash_client}")
     #     print(f"Server file hash: {hash_server}")
-    #
+
     #     assert hash_client == hash_server, (
     #         "SHA256 mismatch: uploaded file is not identical to the original"
     #     )
